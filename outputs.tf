7ccc9981389fe59cc8c4149d1acecc4e23e92805
Brian <briancabbott@gmail.com>
2021-08-17 14:50:57 -0700

    Updated for registry.terraform.io documentation project.

--- outputs.tf
+++ outputs.tf
@@ -6,5 +6,6 @@ locals {
 }
 
 output "policies" {
+  description = "A collection of files containing local policy definitions. These are JSON files residing in the policies directory relative to the command execution path."
   value = local.policies
 }
