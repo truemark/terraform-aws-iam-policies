aa847b749d26e3f55de8a852bd3822f1162de524
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-06-10 11:32:11 -0600

    Fixing formatting

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 locals {
   files = fileset(path.module, "policies/*.json")
   policies = {
-    for file in local.files: replace(file, "/.*\\/(.*).json/", "$1") => file("${path.module}/${file}")
+    for file in local.files : replace(file, "/.*\\/(.*).json/", "$1") => file("${path.module}/${file}")
   }
 }
 
 resource "aws_iam_policy" "this" {
   for_each = toset(var.create_policies)
-  name = each.key
-  policy = local.policies[each.key]
+  name     = each.key
+  policy   = local.policies[each.key]
 }
 
 locals {
   created_policy_arns = {
-    for policy in aws_iam_policy.this: policy.name => policy.arn
+    for policy in aws_iam_policy.this : policy.name => policy.arn
   }
   created_policy_ids = {
-    for policy in aws_iam_policy.this: policy.name => policy.id
+    for policy in aws_iam_policy.this : policy.name => policy.id
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "policies" {
   description = "A collection of files containing local policy definitions. These are JSON files residing in the policies directory relative to the command execution path."
-  value = local.policies
+  value       = local.policies
 }
 
 output "created_policy_arns" {
